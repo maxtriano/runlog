65d21d2d98e1310e93f7a647d8fc8e1e3e4e3282
Max Triano <mtriano234@gmail.com>
2018-10-27 17:11:35 -0400

    Fixes

--- runlog/__main__.py
+++ runlog/__main__.py
@@ -2,18 +2,22 @@ from runlog.run import Run
 import sys, sqlite3
 from datetime import datetime
 conn = sqlite3.connect('runs.db')
-c = conn.curor()
+c = conn.cursor()
 
 c.execute("""CREATE TABLE IF NOT EXISTS runs (
     id integer PRIMARY KEY,
     date integer NOT NULL,
     distance real NOT NULL,
-    duration text NOT NULL),
+    duration text NOT NULL,
     type text,
-    notes text""")
+    notes text );""")
 
 def main():
     print('\n=== runlog ===\n')
+    c.execute("SELECT * FROM runs WHERE ID = (SELECT MAX(ID)  FROM runs);")
+    row = c.fetchall()
+    print(row)
+    print('\n')
     print("1) Add a run\n")
     choice = input('What would you like to do? ')
     
@@ -31,9 +35,8 @@ def add_run():
     print('\n')
     run_notes = input('Notes: ')
 
-    c.execute("""INSERT INTO runs(date, distance, duration, type, notes) 
-                    VALUES(strftime('%s', 'now'), ?, ?, ?, ?)""",
-                    (run_distance, run_time, run_type, run_notes))
+    c.execute("INSERT INTO runs (date, distance, duration, type, notes) VALUES (strftime('%s', 'now'), ?, ?, ?, ?)"
+                    , (run_distance, run_time, run_type, run_notes))
     conn.commit()
 
 if __name__ == '__main__':
