b822bb811645f2a3485b578151c1285647d069b6
Max Triano <mtriano234@gmail.com>
2018-10-27 15:18:09 -0400

    Moved things around, removed deprecated 'has_key'

--- runlog/__main__.py
+++ runlog/__main__.py
@@ -1,9 +1,26 @@
 from .run import Run
 import sys
+from datetime import datetime
+import ZODB, ZODB.FileStorage, BTrees.OOBTree, transaction, persistent
+
+storage=ZODB.FileStorage.FileStorage("runs.fs")
+db=ZODB.DB(storage)
+connection=db.open()
+root = connection.root()
+
+if 'runs' not in root:   
+    root['runs'] = BTrees.OOBTree.BTree()
+
+runs = root['runs']
+
+def add_run(distance, time):
+    runs[datetime.now()] = Run(distance, time)
+    root["runs"] = runs
+    transaction.commit
 
 def main():
-    print('runlog')
-    print('======')
+    print('\n=== runlog ===\n\n')
+    print("1) Add a run")
 
 
 if __name__ == '__main__':
--- runlog/run.py
+++ runlog/run.py
@@ -1,24 +1,17 @@
 from datetime import timedelta
 from datetime import datetime
-import ZODB, ZODB.FileStorage, BTrees.OOBTree, transaction, persistent
+import persistent
 
 class Run(persistent.Persistent):
 
-    def __init__(self, distance, time):
-        self.date = datetime.now()
+    def __init__(self, distance = None, time = None):
         self.distance = distance
-        t = datetime.strptime(time, "%H:%M:%S")
-        self.time = timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
-        self.pace = (time / 60.0) / distance
+        if time:
+            t = datetime.strptime(time, "%H:%M:%S")
+            self.time = timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
+            self.pace = (time / 60.0) / distance
 
-    storage=ZODB.FileStorage.FileStorage("runs.fs")
-    db=ZODB.DB(storage)
-    connection=db.open()
-    root = connection.root()
-    
-    if not root.has_key("runs"):
-        root.runs = BTrees.OOBTree.BTree()
-    
+
 
     
 
